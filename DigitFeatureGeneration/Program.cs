98fdc4f330ddffa4e9db6577b7248e716552bf58
willowcheng <e.wingus@Gmail.com>
2016-04-20 21:10:15 -0400

    Generation tran and test result with proper data header

--- DigitFeatureGeneration/Program.cs
+++ DigitFeatureGeneration/Program.cs
@@ -5,10 +5,10 @@ namespace DigitFeatureGeneration
 {
 	public static class Algorithm
 	{
-		public static string algorithmExample (string id, string number) 
+		public static string algorithmExample (string id, string path, string number = "") 
 		{
 			// Read single binary file
-			var binaryArray = File.ReadAllBytes(Constants.trainingSetPath + id);
+			var binaryArray = File.ReadAllBytes(path + id);
 
 			// Initial array with feature number as row + column
 			int[] binaryResult = new int[Constants.row + Constants.column];
@@ -27,7 +27,13 @@ namespace DigitFeatureGeneration
 			}
 
 			// Convert integer array to string arry and separate by "," to make it readable as csv
-			return id + "," + number + "," + string.Join(",", binaryResult);
+			if (number.Equals("")) {
+				return id + "," + string.Join(",", binaryResult);
+			} 
+			else
+			{
+				return id + "," + number + "," + string.Join(",", binaryResult);
+			}
 		}
 	}
 
@@ -36,10 +42,13 @@ namespace DigitFeatureGeneration
 		public const int row = 28;
 		public const int column = 28;
 		public const string trainReferencePath = "src/train.csv";
+		public const string testReferencePath = "src/sample.csv";
 		public const string trainingSetPath = "src/train/";
+		public const string testSetPath = "src/test/";
 		public const string exportPath = "";
 
-		public const string algorithmExampleResultFileName = "algorithm-example-result.csv";
+		public const string algorithmExampleTrainResultFileName = "algorithm-example-train-result.csv";
+		public const string algorithmExampleTestResultFileName = "algorithm-example-test-result.csv";
 
 		public static void exportHelper (string resultString, string exportFileName) 
 		{
@@ -52,10 +61,11 @@ namespace DigitFeatureGeneration
 
 	class Program
 	{
-		static void Main(string[] args)
+		static void Main()
 		{
 			// 1. Read train.csv as reference
 			String[] trainReference = File.ReadAllText(Constants.trainReferencePath).Split(',', '\n');
+			String[] testReference = File.ReadAllText(Constants.testReferencePath).Split(',', '\n');
 
 			// 2. Apply different algorithms to generate features
 			// Note: Original test score is -2.302585
@@ -63,18 +73,30 @@ namespace DigitFeatureGeneration
 			/* 
 			 * - Example algorithm
 			 */
-			var algorithmExampleResultString = "";
+			var algorithmExampleTrainResultString = "id,number,column1,column2,column3,column4,column5,column6,column7,column8,column9,column10,column11,column12,column13,column14,column15,column16,column17,column18,column19,column20,column21,column22,column23,column24,column25,column26,column27,column28,column29,column30,column31,column32,column33,column34,column35,column36,column37,column38,column39,column40,column41,column42,column43,column44,column45,column46,column47,column48,column49,column50,column51,column52,column53,column54,column55,column56" + "\n";
+			var algorithmExampleTestResultString = "id,column1,column2,column3,column4,column5,column6,column7,column8,column9,column10,column11,column12,column13,column14,column15,column16,column17,column18,column19,column20,column21,column22,column23,column24,column25,column26,column27,column28,column29,column30,column31,column32,column33,column34,column35,column36,column37,column38,column39,column40,column41,column42,column43,column44,column45,column46,column47,column48,column49,column50,column51,column52,column53,column54,column55,column56" + "\n";
 
 			for (var i = 0; i < trainReference.Length; i++) 
 			{
-				if (i % 2 == 0 && i < trainReference.Length - 1) {
+				if (i % 2 == 0 && i < trainReference.Length - 1) 
+				{
 					// Generating 56 features using summation for columns and rows
-					algorithmExampleResultString += Algorithm.algorithmExample (trainReference [i], trainReference [i + 1]) + "\n";
+					algorithmExampleTrainResultString += Algorithm.algorithmExample (trainReference [i], Constants.trainingSetPath, trainReference [i + 1]) + "\n";
+				}
+			}
+
+			for (var i = 0; i < testReference.Length; i++) 
+			{
+				if (i % 11 == 0 && i < testReference.Length - 1) 
+				{
+					// Generating 56 features using summation for columns and rows
+					algorithmExampleTestResultString += Algorithm.algorithmExample (testReference [i], Constants.testSetPath) + "\n";
 				}
 			}
 
 			// 3. Export csv files for different algorithms
-			Constants.exportHelper(algorithmExampleResultString, Constants.algorithmExampleResultFileName);
+			Constants.exportHelper(algorithmExampleTrainResultString, Constants.algorithmExampleTrainResultFileName);
+			Constants.exportHelper(algorithmExampleTestResultString, Constants.algorithmExampleTestResultFileName);
 		}
 	}
 }
