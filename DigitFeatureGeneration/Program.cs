2e0e878ffc2698c277b87b735361121f17c00259
willowcheng <e.wingus@Gmail.com>
2016-05-12 14:08:36 -0400

    Update for test features generation

--- DigitFeatureGeneration/Program.cs
+++ DigitFeatureGeneration/Program.cs
@@ -82,7 +82,7 @@ namespace DigitFeatureGeneration
 		static void Main()
 		{
 			// 1. Read train.csv as reference
-			String[] trainReference = File.ReadAllText(Constants.trainReferencePath).Split(',', '\n');
+//			String[] trainReference = File.ReadAllText(Constants.trainReferencePath).Split(',', '\n');
 			String[] testReference = File.ReadAllText(Constants.testReferencePath).Split(',', '\n');
 
 			// 2. Apply different algorithms to generate features
@@ -91,52 +91,52 @@ namespace DigitFeatureGeneration
 			/* 
 			 * - Example Algorithm
 			 */
-			var algorithmExampleTrainResultString = "id,number,column1,column2,column3,column4,column5,column6,column7,column8,column9,column10,column11,column12,column13,column14,column15,column16,column17,column18,column19,column20,column21,column22,column23,column24,column25,column26,column27,column28,column29,column30,column31,column32,column33,column34,column35,column36,column37,column38,column39,column40,column41,column42,column43,column44,column45,column46,column47,column48,column49,column50,column51,column52,column53,column54,column55,column56";
-			var algorithmExampleTestResultString = "id,column1,column2,column3,column4,column5,column6,column7,column8,column9,column10,column11,column12,column13,column14,column15,column16,column17,column18,column19,column20,column21,column22,column23,column24,column25,column26,column27,column28,column29,column30,column31,column32,column33,column34,column35,column36,column37,column38,column39,column40,column41,column42,column43,column44,column45,column46,column47,column48,column49,column50,column51,column52,column53,column54,column55,column56";
-
-			for (var i = 0; i < trainReference.Length; i++) 
-			{
-					
-
-				if (i % 2 == 0 && i < trainReference.Length - 1) 
-				{
-					// Generating 56 features using summation for columns and rows
-					algorithmExampleTrainResultString += "\n" + Algorithm.algorithmExample (trainReference [i], Constants.trainingSetPath, trainReference [i + 1]);
-				}
-			}
-
-			for (var i = 0; i < testReference.Length; i++) 
-			{
-
-				if (i % 11 == 0 && i < testReference.Length - 1) 
-				{
-					// Generating 56 features using summation for columns and rows
-					algorithmExampleTestResultString += "\n" + Algorithm.algorithmExample (testReference [i], Constants.testSetPath);
-				}
-			}
+//			var algorithmExampleTrainResultString = "id,number,column1,column2,column3,column4,column5,column6,column7,column8,column9,column10,column11,column12,column13,column14,column15,column16,column17,column18,column19,column20,column21,column22,column23,column24,column25,column26,column27,column28,column29,column30,column31,column32,column33,column34,column35,column36,column37,column38,column39,column40,column41,column42,column43,column44,column45,column46,column47,column48,column49,column50,column51,column52,column53,column54,column55,column56";
+//			var algorithmExampleTestResultString = "id,column1,column2,column3,column4,column5,column6,column7,column8,column9,column10,column11,column12,column13,column14,column15,column16,column17,column18,column19,column20,column21,column22,column23,column24,column25,column26,column27,column28,column29,column30,column31,column32,column33,column34,column35,column36,column37,column38,column39,column40,column41,column42,column43,column44,column45,column46,column47,column48,column49,column50,column51,column52,column53,column54,column55,column56";
+//
+//			for (var i = 0; i < trainReference.Length; i++) 
+//			{
+//					
+//
+//				if (i % 2 == 0 && i < trainReference.Length - 1) 
+//				{
+//					// Generating 56 features using summation for columns and rows
+//					algorithmExampleTrainResultString += "\n" + Algorithm.algorithmExample (trainReference [i], Constants.trainingSetPath, trainReference [i + 1]);
+//				}
+//			}
+//
+//			for (var i = 0; i < testReference.Length; i++) 
+//			{
+//
+//				if (i % 11 == 0 && i < testReference.Length - 1) 
+//				{
+//					// Generating 56 features using summation for columns and rows
+//					algorithmExampleTestResultString += "\n" + Algorithm.algorithmExample (testReference [i], Constants.testSetPath);
+//				}
+//			}
 
 			/*
 			 * - All Number Algorithm
 			 */
-			var algorithmAllNumberTrainResultString = "id,number";
+//			var algorithmAllNumberTrainResultString = "id,number";
 			var algorithmAllNumberTestResultString = "id";
 
 			for (var i = 0; i < Constants.row * Constants.column; i++) 
 			{
-				algorithmAllNumberTrainResultString += ",column" + (i + 1).ToString();
+//				algorithmAllNumberTrainResultString += ",column" + (i + 1).ToString();
 				algorithmAllNumberTestResultString += ",column" + (i + 1).ToString();
 			}
 
-			for (var i = 0; i < trainReference.Length; i++) 
-			{
-
-
-				if (i % 2 == 0 && i < trainReference.Length - 1) 
-				{
-					// Generating 56 features using summation for columns and rows
-					algorithmAllNumberTrainResultString += "\n" + Algorithm.algorithmAllNumber (trainReference [i], Constants.trainingSetPath, trainReference [i + 1]);
-				}
-			}
+//			for (var i = 0; i < trainReference.Length; i++) 
+//			{
+//
+//
+//				if (i % 2 == 0 && i < trainReference.Length - 1) 
+//				{
+//					// Generating 56 features using summation for columns and rows
+//					algorithmAllNumberTrainResultString += "\n" + Algorithm.algorithmAllNumber (trainReference [i], Constants.trainingSetPath, trainReference [i + 1]);
+//				}
+//			}
 
 			for (var i = 0; i < testReference.Length; i++) 
 			{
@@ -151,10 +151,10 @@ namespace DigitFeatureGeneration
 
 
 			// 3. Export csv files for different algorithms
-			Constants.exportHelper(algorithmExampleTrainResultString, Constants.algorithmExampleTrainResultFileName);
-			Constants.exportHelper(algorithmExampleTestResultString, Constants.algorithmExampleTestResultFileName);
+//			Constants.exportHelper(algorithmExampleTrainResultString, Constants.algorithmExampleTrainResultFileName);
+//			Constants.exportHelper(algorithmExampleTestResultString, Constants.algorithmExampleTestResultFileName);
 
-			Constants.exportHelper(algorithmAllNumberTrainResultString, Constants.algorithmAllNumberTrainResultFileName);
+//			Constants.exportHelper(algorithmAllNumberTrainResultString, Constants.algorithmAllNumberTrainResultFileName);
 			Constants.exportHelper(algorithmAllNumberTestResultString, Constants.algorithmAllNumberTestResultFileName);
 		}
 	}
